41d9d6e004f1e2023028d42fd54f44eb7c9684bc
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2022-07-08 10:50:04 +0700

    Update setupvps.sh

--- setupvps.sh
+++ setupvps.sh
@@ -1,13 +1,13 @@
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt-get install docker-ce
-sudo apt install docker-compose
-sudo apt install tree
+#!/bin/bash
+apt-get update && apt-get upgrade
+apt-get install curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt-get install docker-ce
+apt install docker-compose
+apt install tree
+apt install nmap
 git clone https://github.com/dreammaker97/portainer
 cd portainer
-sudo docker-compose up
-
+docker-compose up
