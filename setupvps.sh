80c4c17c99c48235bcb026c76be42c090700cb6c
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2022-08-13 22:58:38 +0700

    Update setupvps.sh

--- setupvps.sh
+++ setupvps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && apt-get upgrade
 sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt-get install docker-ce
